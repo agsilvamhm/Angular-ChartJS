7cd6fd47d504ac19963f5f4fcecf98a00c2682bc
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-01-23 20:29:00 -0300

    Guardando o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-ChartJS';
+//  @ViewChild("meuCanvas", { static: true })
+//  elemento: ElementRef;
+
+
+ //  ngOnInit(): void {
+ //   new Chart(this.elemento.nativeElement, {
+ //     type:'line',
+ //     data: {
+ //       labels:['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+ //       datasets :[{
+ //         data: [85, 26, 13, 56, 16]
+ //      }]
+ //    }
+ //   });
+ // }
 }
